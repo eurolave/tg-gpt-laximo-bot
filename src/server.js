b8c95d54b4678dd97928fd798a51c1eccef8de5b
eurolave <eurolave@gmail.com>
2025-09-21 11:51:37 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import { Telegraf } from 'telegraf';
 import OpenAI from 'openai';
-import { handleUserText, handleCallback, startFlow } from '../core/orchestrator.js';
+import { handleUserText, handleCallback, startFlow } from './core/orchestrator.js';
 import { mainMenu, backMenu } from './keyboards.js';
 
 const app = express();
